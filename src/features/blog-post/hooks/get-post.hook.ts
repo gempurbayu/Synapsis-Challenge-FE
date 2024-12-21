880f6be01bcb2f4dcf225295ad6d49b3009cbdeb
Gempur Bayu <gempur@rekadia.co.id>
2024-12-21 15:17:55 +0700

    feat: search post by title

--- src/features/blog-post/hooks/get-post.hook.ts
+++ src/features/blog-post/hooks/get-post.hook.ts
@@ -1,9 +1,13 @@
 import { useQuery } from '@tanstack/react-query';
 import getPostsApi from '../api/get-post.api';
 
-export default function useGetPosts(page: number, perPage: number) {
+export default function useGetPosts(
+  page: number,
+  perPage: number,
+  search?: string
+) {
   return useQuery({
-    queryFn: () => getPostsApi(page, perPage),
+    queryFn: () => getPostsApi(page, perPage, search),
     queryKey: ['posts'],
     enabled: !page && !perPage,
   });
